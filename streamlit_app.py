180a8839fd0952b607cfecda2332e06941486990
carolina-grille <72971896+carolina-grille@users.noreply.github.com>
2023-11-27 12:59:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit
 
-streamlit.title('My parents new healthy diner')
+streamlit.title('My mom's new healthy diner')
 
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled ree-Range Egg')
+streamlit.header('Breakfast favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🐔 Hard-Boiled ree-Range Egg')
+streamlit.text(' 🥑🍞Avocado toast')
+ 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
